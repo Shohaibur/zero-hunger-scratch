d9a64062649f1665eceb5f085d263d270c5f3bf8
Shoaib <shoaibrochi9@gmail.com>
2023-11-23 04:15:05 +0600

    Add project files.

---
+++ zero-hunger-scratch/Controllers/NgoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using zero_hunger_scratch.Models;
+
+namespace zero_hunger_scratch.Controllers
+{
+    public class NgoController : Controller
+    {
+        private readonly zerohungerEntities db = new zerohungerEntities();
+
+        // GET: Ngo
+        public ActionResult NRestaurants()
+        {
+            List<RestaurantDashboard> restaurants = db.RestaurantDashboards.ToList();
+            return View(restaurants);
+        }
+
+        [HttpPost]
+        public ActionResult NRestaurants(RestaurantNGOEnd e)
+        {
+            var db = new zerohungerEntities();
+
+            // Create a new instance of the other model
+            var data = new RestaurantNGOEnd()
+            {
+                // Set the properties of the other model with the values from the form
+                RestaurantDashboardId =e.RestaurantDashboardId,
+                RestaurantName = e.RestaurantName,
+                SelectedFood = e.SelectedFood,
+                ByTime = (TimeSpan)e.ByTime,
+                Contact = e.Contact,
+                RestaurantLocation = e.RestaurantLocation
+                
+            };
+
+            // Add the new instance to the corresponding DbSet
+            db.RestaurantNGOEnds.Add(data);
+
+            // Save the changes to the database
+            db.SaveChanges();
+
+            // Redirect to the same action or another action as needed
+            return RedirectToAction("Index","Home");
+        }
+
+    }
+
+}
---
+++ zero-hunger-scratch/Controllers/RestaurantController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using zero_hunger_scratch.Controllers;
+using zero_hunger_scratch.Models;
+
+namespace zero_hunger_scratch.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // GET: Restaurant
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(RestaurantDashboard r)
+        {
+            var db = new zerohungerEntities();
+            db.RestaurantDashboards.Add(r);
+            db.SaveChanges();
+            return RedirectToAction("RestaurantDashboard", new {id=r.RestaurantDashboardId});
+        }
+        [HttpGet]
+        public ActionResult RestaurantDashboard(int id)
+        {
+            var db = new zerohungerEntities();
+            var data = db.RestaurantDashboards.Find(id);
+
+                       
+
+            return View(data);
+        }
+       
+        [HttpPost]
+        public ActionResult RestaurantDashboard(int id, string SelectFood, TimeSpan ByTime,int Contact)
+        {
+            var db = new zerohungerEntities();
+
+            // Find the restaurant based on the provided id
+            var restaurant = db.RestaurantDashboards.Find(id);
+
+            // Update the SelectFood and ByTime properties with the values from the form
+            restaurant.SelectFood = SelectFood;
+            restaurant.ByTime = ByTime;
+            restaurant.Contact = Contact;
+
+            // Save changes to the database
+            db.SaveChanges();
+
+            // Redirect to the same action or another action as needed
+            return RedirectToAction("Confirmed");
+        }
+        public ActionResult Confirmed()
+        {
+            return View();
+        }
+
+
+
+    }
+}
